f60af6030f3cc705ca6f2fdebbcd80f6dc6137a5
Lucifer <alex-orl1@mail.ru>
2021-03-09 13:11:23 +0300

    Delete p2 directory

--- p2/test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require_relative 'funcs.rb'
-
-class TestString < Minitest::Test
-    def test_change_example
-      assert_equal 'темп кек лул1', change('лол нигга кек лул1 лул2', 'темп')
-    end
-end
